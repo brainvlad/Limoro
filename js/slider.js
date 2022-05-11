d4b7caf3ab2f7155bcd97f60b2c9beaa31eb77e7
Vladislav Baranov <wladiik@gmail.com>
2022-05-11 12:28:32 +0300

    first commit

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".welcome-slider").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeep: 100,
+    pauseOnFocus: true,
+    pauseOnHover: false,
+    pasusOnDotsHover: false,
+  });
+});
